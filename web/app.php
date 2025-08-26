a88bfd269120189d6b69b70a07307a2f1f5725b4
hichamtahli <hicham.tahli@gmail.com>
2025-08-26 15:02:00 +0100

    parametres

--- web/app.php
+++ web/app.php
@@ -6,20 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../app/AppKernel.php';
 
-// --- Proxies/Hosts de confiance (Kinsta via Nginx/Load Balancer) ---
-// Permet à Symfony de lire correctement X-Forwarded-Proto/Host/Port
-// et d'éviter les boucles HTTP<->HTTPS / locale<->locale.
-Request::setTrustedProxies(
-    ['0.0.0.0/0'], // en container, on peut faire confiance à tout (ou liste IP LB Kinsta si connue)
-    Request::HEADER_X_FORWARDED_ALL
-);
-
-// restreins l’host attendu (ajoute tes domaines si besoin)
-Request::setTrustedHosts([
-    '^skigailuron-75pio\.kinsta\.app$',
-    // '^www\.ton-domaine\.com$',
-    // '^ton-domaine\.com$',
-]);
 
 // Autorise la surcharge de méthode HTTP via _method
 Request::enableHttpMethodParameterOverride();
